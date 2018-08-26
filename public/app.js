ea35c238fbb32568526d309f401f6bbcdb7ffc9d
dannyhz92 <d.hernandez@lafilm.edu>
2018-08-25 23:19:36 -0700

    commented out some code that was breaking app.js. Going to rely on cdn for velocity. Got Velocity working, added animations to second major div. And changed css

--- public/app.js
+++ public/app.js
@@ -1,6 +1,22 @@
-import Velocity from 'velocity-animate';
+// import Velocity from "velocity-animate";
 
-Velocity(document.getElementById("hometron"), { opacity: 0.5 }, { duration: 1000 });
+// Velocity(document.getElementById("hometron"), { opacity: 0.5 }, { duration: 1000 });
+
+var isSecondPartAnimated = false;
+
+setTimeout(function() {
+  $("#tron")
+    .velocity("transition.slideUpIn", { stagger: 250 })
+
+}, 500);
+$(window).scroll(function(){
+
+if($(window).scrollTop() > 20 && !isSecondPartAnimated) {
+  $("#secondsection")
+  .velocity("slideDown", { duration: 1500 })
+  isSecondPartAnimated = true;
+}
+});
 
 // document.getElementById("hermans").style.color = green;
 
